a7bcce70b7a9ead056658f82fc3357b7186d0998
Adil Ali <adilalicm@gmail.com>
2026-07-14 15:03:11 +0530

    Create Spring Boot project with configured pom.xml

---
+++ backend/src/main/java/com/xnullcode/brol/BrolApplication.java
@@ -0,0 +1,13 @@
+package com.xnullcode.brol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/xnullcode/brol/BrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xnullcode.brol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
